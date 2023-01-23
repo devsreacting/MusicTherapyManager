d11a366839e3840e3723105d7d68a9880c6e349b
Lucas Weber <375474@wegmans.com>
2023-01-23 14:54:07 -0500

    navbar basic routing and folder setup

--- music_therapy_manager/app/layout.js
+++ music_therapy_manager/app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css'
-
+import Navbar from './navbar'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -7,7 +7,10 @@ export default function RootLayout({ children }) {
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
+
       <head />
+      <Navbar />
+      
       <body>{children}</body>
     </html>
   )
