4b507e11347372747d12d192950a7c6cc6a5c366
Lucas Weber <weberlucasdev@gmail.com>
2023-01-27 12:02:29 -0500

    cleanup and vercel deploy

--- music_therapy_manager/app/page.tsx
+++ music_therapy_manager/app/page.tsx
@@ -1,10 +1,10 @@
 "use client"
-import { Inter } from '@next/font/google'
+
 import { useAuth } from '../context/AuthContext'
 import Login from "../components/Login"
 import UserDashboard from "../components/UserDashboard"
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   const { currentUser } = useAuth()
