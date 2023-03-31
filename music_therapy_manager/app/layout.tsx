6aa017987b199733796eff612e36d3212d85e2dd
Luke Weber <weberlucasdev@gmail.com>
2023-03-31 19:02:36 +0000

    CSS mostly working

--- music_therapy_manager/app/layout.tsx
+++ music_therapy_manager/app/layout.tsx
@@ -1,37 +1,40 @@
 "use client"
-import './globals.css'
-import Head from 'next/head'
-import NavbarSidebarLayout from "../layouts/navbar-sidebar";
-import Context from "./Context";
-import { PropsWithChildren } from "react";
+import './globals.css';
+import Head from 'next/head';
+import NavbarSidebarLayout from '../layouts/navbar-sidebar';
+
+import { PropsWithChildren } from 'react';
 import { AuthProvider } from '../context/AuthContext';
-import BodyWrapper from '../components/BodyWrapper';
+import { StrictMode } from "react";
+import { Flowbite } from "flowbite-react";
 import { NextPage } from 'next';
+import theme from "./flowbite-theme";
 
+const RootLayout: NextPage<PropsWithChildren> = ({ children }) => (
+  <html lang="en">
+    {/*
+      <head /> will contain the components returned by the nearest parent
+      head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+    */}
 
-const RootLayout: NextPage<PropsWithChildren> = function ({ children }) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <Head children={undefined} />
 
-      <Head children={undefined} />
+    <body>
+      
+      <AuthProvider>
+        <StrictMode>
+      <Flowbite theme={{ theme }}> 
+        <NavbarSidebarLayout>
+       
+            {children}
+    
+        </NavbarSidebarLayout>
+        </Flowbite>
+        </StrictMode>
+      </AuthProvider>
+     
+    </body>
+  </html>
+);
 
-
-      <body>
-
-        <AuthProvider>
-          <BodyWrapper>
-            <NavbarSidebarLayout>
-              <Context>{children}</Context>
-              </NavbarSidebarLayout>
-          </BodyWrapper>
-        </AuthProvider>
-
-      </body>
-    </html>
-  )
-}
-export default RootLayout
+export default RootLayout;
--- music_therapy_manager/app/page.tsx
+++ music_therapy_manager/app/page.tsx
@@ -7,7 +7,7 @@ import Login from "../components/Login"
 
 
 export default function Home() {
-  const { currentUser } = useAuth()
+ // const { currentUser } = useAuth()
 
   return (
     <div>
